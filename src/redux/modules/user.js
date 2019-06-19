fd0686ff6a242ad73874414737da29620da92eed
edward_chen <edward_chen@zc-campus.com>
2019-06-19 14:30:37 +0800

    功能开发完成

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -5,12 +5,13 @@ import {
   TO_PAY_TYPE,
   AVAILABLE_TYPE,
   REFUND_TYPE,
-  getOrderById,
+  getAllOrders,
   types as orderTypes,
   actions as orderActions
 } from "./entities/orders";
 import { actions as commentActions } from "./entities/comments";
 import { combineReducers } from "redux";
+import { createSelector } from "reselect";
 
 const typeToKey = {
   [TO_PAY_TYPE]: "toPayIds",
@@ -285,14 +286,18 @@ export default reducer;
 // selectors
 export const getCurrentTab = state => state.user.currentTab;
 
-export const getOrders = state => {
-  const key = ["ids", "toPayIds", "availableIds", "refundIds"][
-    state.user.currentTab
-  ];
-  return state.user.orders[key].map(id => {
-    return getOrderById(state, id);
-  });
-};
+const getUserOrders = state => state.user.orders;
+
+export const getOrders = createSelector(
+  [getCurrentTab, getUserOrders, getAllOrders],
+  (tabIndex, userOrders, orders) => {
+    const key = ["ids", "toPayIds", "availableIds", "refundIds"][tabIndex];
+    const orderIds = userOrders[key];
+    return orderIds.map(id => {
+      return orders[id];
+    });
+  }
+);
 
 // 获取正在删除的订单id
 export const getDeletingOrderId = state => {
